4086d16a0743ea5d81dc0fd295b1b00e077589c5
xwj2003525 <xwj2003525@gmail.com>
2024-11-19 13:12:05 +0800

    更改接口，更加简洁

--- time/time.h
+++ time/time.h
@@ -14,89 +14,72 @@ namespace x {
 
         class StampView {
         public:
-             uint16_t year;
-             uint8_t month;
-             uint8_t day;
-             uint8_t hour;
-             uint8_t minute;
-             uint8_t second;
-             uint16_t millisecond;
-
+            uint16_t year;
+            uint8_t month;
+            uint8_t day;
+            uint8_t hour;
+            uint8_t minute;
+            uint8_t second;
+            uint16_t millisecond;
             std::string toString() const;
         };
 
         class GapView {
         public:
+            uint8_t day;
+            uint8_t hour;
+            uint8_t minute;
+            uint8_t second;
+            uint16_t millisecond;
             std::string toString() const;
-
-            const uint8_t day;
-            const uint8_t hour;
-            const uint8_t minute;
-            const uint8_t second;
-            const uint16_t millisecond;
-
         };
 
         class Gap;
 
         class Stamp {
         public:
-            static const uint64_t MIN_MILLISECONDS_SINCE_EPOCH;
-            static const uint64_t MAX_MILLISECONDS_SINCE_EPOCH;
-            static const uint16_t MAX_YEAR;
-            static const uint16_t MIN_YEAR;
-
-            Stamp(const StampView&);
-            Stamp(const Stamp&);
-
-            static Stamp Max();
-            static Stamp Min();
             static Stamp Now();
-            static Stamp DateTime(int year, int month, int day, int hour, int minute, int second, int millisecond);
+            static Stamp When(int year, int month, int day, int hour, int minute, int second, int millisecond);
+
+            Stamp(uint64_t);
+            Stamp(const Stamp&);
+            uint64_t MilliSecondsSinceEpoch()const;
+            StampView View()const;
 
             Gap operator-(const Stamp&) const;
             Stamp operator+(const Gap&) const;
             Stamp& operator+=(const Gap&);
             Stamp operator-(const Gap&) const;
             Stamp& operator-=(const Gap&);
-
-            StampView View()const;
         protected:
-            friend class Gap;
             Stamp() = delete;
-            Stamp(uint64_t);
-
             uint64_t milliseconds_since_epoch;
         };
 
         class Gap {
         public:
-            static const uint64_t MAX_MILLISECONDS;
-            static const uint64_t MIN_MILLISECONDS;
+            static Gap Weeks(uint64_t);
+            static Gap Days(uint64_t);
+            static Gap Hours(uint64_t);
+            static Gap Minutes(uint64_t);
+            static Gap Seconds(uint64_t);
+            static Gap MilliSeconds(uint64_t);
 
             Gap(uint64_t);
             Gap(const Gap&);
 
-            static Gap Max();
-            static Gap Min();
+            GapView View()const;
+            uint64_t MilliSeconds()const;
 
             Stamp operator+(const Stamp&)const;
             Gap operator+(const Gap&) const;
             Gap& operator+=(const Gap&);
-
-            GapView View()const;
         protected:
-            friend class Stamp;
             Gap() = delete;
             uint64_t milliseconds;
         };
 
+
         bool isValidDateTime(int year, int month, int day, int hour, int minute, int second, int millisecond);
-        Gap Weeks(uint64_t);
-        Gap Days(uint64_t);
-        Gap Hours(uint64_t);
-        Gap Minutes(uint64_t);
-        Gap Seconds(uint64_t);
-        Gap MilliSeconds(uint64_t);
     };
 };
